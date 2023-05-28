0081c80032337bcf6a783cf30086f58432fa92f1
Jose <josemorenodev@outlook.com>
2023-05-28 14:12:51 -0400

    Finished test string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num <0) {
+        return "ERROR";
+    }
+    let count=1;
+    let newString="";
+    while (count <= num) {
+        newString = string + newString;
+        count++;
+        
+    }
+    return newString;
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
